52da8da8ab85c2c333890d7db55de09b2fa67662
Grace <gkchan09@gmail.com>
2017-06-27 22:00:05 +0000

    Add tables (users, levels, modules, functions)

--- model.py
+++ model.py
@@ -6,3 +6,58 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+# Model definitions.
+
+
+
+# NOTE: add backref
+
+
+
+class User(db.Model):
+    """Creates a table for user info."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(64), nullable=False)
+    first_name = db.Column(db.String(64), nullable=False)
+    last_name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+
+
+class Level(db.Model):
+    """Creates a table for tracking user progress."""
+
+    __tablename__ = "levels"
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True)
+    level = db.Column(db.Integer, nullable=False, default=0)
+    points = db.Column(db.Integer, nullable=False, default=0)
+
+
+class Module(db.Model):
+    """Creates a table of modules."""
+
+    __tablename__ = "modules"
+
+    module_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(256), nullable=False)
+    additional_info = db.Column(db.Text, nullable=True)
+
+
+class Functions(db.Model):
+    """Creates a table for functions."""
+
+    __tablename__ = "functions"
+
+    function_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(256), nullable=False)
+    additional_info = db.Column(db.Text, nullable=True)
+
+
+
+
+
