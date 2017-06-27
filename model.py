6a9a3cd3eaffabb7077b7c3ef592b22248efa20c
Grace <gkchan09@gmail.com>
2017-06-27 00:33:35 +0000

    Initial commit

---
+++ model.py
@@ -0,0 +1,8 @@
+"""Database functions for coding tool project."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+# This connects to a PostgreSQL database and uses the Flask-SQLAlchemy library.
+
+db = SQLAlchemy()
+
