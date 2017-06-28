777296e75271c7755bbfa2b74de7d38e39956b84
Grace <gkchan09@gmail.com>
2017-06-28 19:44:26 +0000

    Add functionality for loading levels, modules, and functions into tables

--- seed.py
+++ seed.py
@@ -34,6 +34,79 @@ def load_users():
     print "Users loaded."
 
 
+def load_levels():
+    """Load level data to database."""
+
+    print "Levels"
+
+    # delete levels before data gets added to avoid duplicate info
+    Level.query.delete()
+
+    # insert data from seed_level
+    with open("seed_data/seed_level") as level_data:
+        for row in level_data:
+            user_id, level, points = row.rstrip().split("|")
+            level = Level(user_id=user_id, level=level, points=points)
+
+            # add level to session     
+            db.session.add(level)
+
+    # commit changes
+    db.session.commit()
+
+    print "Levels loaded."
+
+
+def load_modules():
+    """Load modules data to database."""
+
+    print "Modules"
+
+    # delete modules before data gets added to avoid duplicate info
+    Module.query.delete()
+
+    # insert data from seed_module
+    with open("seed_data/seed_module") as module_data:
+        for row in module_data:
+            name, description, additional_info = row.rstrip().split("|")
+            module = Module(name=name, description=description, additional_info=additional_info)
+
+            # add module to session     
+            db.session.add(module)
+
+    # commit changes
+    db.session.commit()
+
+    print "Modules loaded."
+
+
+def load_functions():
+    """Load functions data to database."""
+
+    print "Functions"
+
+    # delete modules before data gets added to avoid duplicate info
+    Function.query.delete()
+
+    # insert data from seed_function
+    with open("seed_data/seed_function") as function_data:
+        for row in function_data:
+            name, description, additional_info, sample_code, output = row.rstrip().split("|")
+            function = Function(name=name, 
+                                description=description, 
+                                additional_info=additional_info, 
+                                sample_code=sample_code, 
+                                output=output)
+
+            # add function to session     
+            db.session.add(function)
+
+    # commit changes
+    db.session.commit()
+
+    print "Function loaded."
+
+
 if __name__ == "__main__":
 
     connect_to_db(app)
@@ -43,5 +116,8 @@ if __name__ == "__main__":
 
     # Load data
     load_users()
+    load_levels()
+    load_modules()
+    load_functions()
 
 
