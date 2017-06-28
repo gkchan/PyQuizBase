e00b6b802ba3dedc4ea264e17413b8e9ab0e7b23
Grace <gkchan09@gmail.com>
2017-06-28 18:59:31 +0000

    Add function to load users data to table

--- seed.py
+++ seed.py
@@ -6,8 +6,13 @@ from model import User, Level, Module, Function
 # import database necessities from model module
 from model import db, connect_to_db
 
+
+from flask import Flask
+app = Flask(__name__)
+
+
 def load_users():
-    """Load seed_user into database."""
+    """Load user data into database."""
 
     print "Users"
 
@@ -17,9 +22,7 @@ def load_users():
     # insert data from seed_user
     with open("seed_data/seed_user") as user_data:
         for row in user_data:
-            print user_data
             username, first_name, last_name, email = row.rstrip().split("|")
-
             user = User(username=username, first_name=first_name, last_name=last_name, email=email)
 
             # add user to session     
@@ -31,5 +34,14 @@ def load_users():
     print "Users loaded."
 
 
+if __name__ == "__main__":
+
+    connect_to_db(app)
+
+    # Create tables if not created
+    db.create_all()
+
+    # Load data
+    load_users()
 
 
