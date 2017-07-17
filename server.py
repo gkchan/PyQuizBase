7cb0bd225b3361871d6dbff777f75f068b523179
Grace <gkchan09@gmail.com>
2017-07-17 19:10:30 +0000

    Refactor server.py, breaking up functions with 2 methods

--- server.py
+++ server.py
@@ -73,32 +73,33 @@ def register_user():
     return render_template("homepage.html")
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route("/login", methods=["GET"])
+def show_login():
+    """Show login page"""
+
+    return render_template("login.html")
+
+
+@app.route("/login", methods=["POST"])
 def login():
-    """Login user"""
+    """Login"""
 
-    if request.method == "GET":
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-        return render_template("login.html")
+    user = User.query.filter_by(username=username, password=password).first()
+    if user:
+        session["current_user"] = username
+        flash("Welcome. You are logged in as {}".format(username))
+        
+        # shows percentage of progress towards next level
+        remainder = user.levels.points % 5 
+        progress = remainder * 100 / 5
+        session["progress"] = progress
+    else:
+        flash("Username or password does not match. Please try again.")
 
-    elif request.method == "POST":
-
-        username = request.form.get("username")
-        password = request.form.get("password")
-
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            session["current_user"] = username
-            flash("Welcome. You are logged in as {}".format(username))
-            
-            # shows percentage of progress towards next level
-            remainder = user.levels.points % 5 
-            progress = remainder * 100 / 5
-            session["progress"] = progress
-        else:
-            flash("Username or password does not match. Please try again.")
-
-        return redirect("/{}/dashboard".format(username))
+    return redirect("/{}/dashboard".format(username))
 
 
 @app.route("/logout")
@@ -155,105 +156,103 @@ def show_study_notes(username):
                             empty_mod=empty_mod)
 
 
-@app.route("/<username>/addmodules", methods=["GET", "POST"])
-def add_modules(username):
-    """Add function/module information"""
+@app.route("/<username>/addmodules", methods=["GET"])
+def show_add_modules(username):
+    """Display add modules page"""
 
     # currently only handles basic adding of info
 
     # verify_user(username)
 
-    if request.method == "GET":
+    return render_template("add_modules.html", username=username)
 
-        return render_template("add_modules.html", username=username)
 
-    elif request.method == "POST":
+@app.route("/<username>/addmodules", methods=["POST"])
+def add_modules(username):
+    """Add function/module information"""
 
-        mname = request.form.get("mname")
-        mdesc = request.form.get("mdesc")
-        fname = request.form.get("fname")
-        fdesc = request.form.get("fdesc")
-        samplecode = request.form.get("samplecode")
-        output = request.form.get("output")
+    mname = request.form.get("mname")
+    mdesc = request.form.get("mdesc")
+    fname = request.form.get("fname")
+    fdesc = request.form.get("fdesc")
+    samplecode = request.form.get("samplecode")
+    output = request.form.get("output")
 
-        # fetch user to get user_id
-        user = User.query.filter_by(username=username).first()
+    # fetch user to get user_id
+    user = User.query.filter_by(username=username).first()
 
-        module = Module(name=mname,
-                        description=mdesc,
-                        user_id=user.user_id)
+    module = Module(name=mname,
+                    description=mdesc,
+                    user_id=user.user_id)
 
-        db.session.add(module)
+    db.session.add(module)
 
-        # fetch module to get module_id
-        module = Module.query.filter_by(name=mname,
-                                        description=mdesc,
-                                        user_id=user.user_id).first()
+    # fetch module to get module_id
+    module = Module.query.filter_by(name=mname,
+                                    description=mdesc,
+                                    user_id=user.user_id).first()
 
-        function = Function(name=fname, 
-                            description=fdesc, 
-                            sample_code=samplecode, 
-                            output=output,
-                            user_id=user.user_id,
-                            module_id=module.module_id)
+    function = Function(name=fname, 
+                        description=fdesc, 
+                        sample_code=samplecode, 
+                        output=output,
+                        user_id=user.user_id,
+                        module_id=module.module_id)
 
-        db.session.add(function)
+    db.session.add(function)
+    db.session.commit()
+
+    flash("Your notes have been added.")
+
+    return redirect("/{}/studynotes".format(username))
+
+
+@app.route("/quiz", methods=["GET"])
+def show_question():
+    """Displays question to answer"""
+
+    # chooses a function entry and asks a question
+    question, input_code, answer, answer_choices = ask_question()
+    session["answer"] = answer
+    session["answer_choices"] = answer_choices
+
+    # print "session answer:", session["answer"]
+    # print answer_choices
+
+    return render_template("question.html", 
+                            question=question, 
+                            input_code=input_code, 
+                            answer_choices=enumerate(answer_choices))
+
+
+@app.route("/quiz", methods=["POST"])
+def process_question():
+    """Processes student answer and displays results with answer."""
+
+    user_answer = request.form.get("useranswer")
+
+    # print "user_answer:", user_answer
+    
+    # User answer is passed back from the form as a number corresponding 
+    # to the index of the list of answers.
+    if session["answer_choices"][int(user_answer)] == session["answer"]:
+        result = "correct!"
+        user = User.query.filter_by(username=session["current_user"]).first()
+        user.levels.points += 1
+        if user.levels.points % 5 == 0:
+            user.levels.level = user.levels.points/5
+            flash("CONGRATULATIONS!!! You've reached level {}".format(user.levels.level))
         db.session.commit()
+        # print user.levels.points 
 
-        flash("Your notes have been added.")
-
-        return redirect("/{}/studynotes".format(username))
-
-
-@app.route("/quiz", methods=["GET", "POST"])
-def display_question():
-    """Displays question to answer and then displays results with answer."""
-
-    if request.method == "GET":
-
-        # chooses a function entry and asks a question
-        question, input_code, answer, answer_choices = ask_question()
-        session["answer"] = answer
-        session["answer_choices"] = answer_choices
-
-        # print "session answer:", session["answer"]
-        # print answer_choices
-
-        return render_template("question.html", 
-                                question=question, 
-                                input_code=input_code, 
-                                answer_choices=enumerate(answer_choices))
-
-    elif request.method == "POST":
-
-        user_answer = request.form.get("useranswer")
-
-        # print "user_answer:", user_answer
-        
-        # User answer is passed back from the form as a number corresponding 
-        # to the index of the list of answers.
-        if session["answer_choices"][int(user_answer)] == session["answer"]:
-            result = "correct!"
-            user = User.query.filter_by(username=session["current_user"]).first()
-            user.levels.points += 1
-            if user.levels.points % 5 == 0:
-                user.levels.level = user.levels.points/5
-                flash("CONGRATULATIONS!!! You've reached level {}".format(user.levels.level))
-            db.session.commit()
-            # print user.levels.points 
-
-            remainder = user.levels.points % 5 
-            progress = remainder * 100 / 5
-            session["progress"] = progress
-
-        else:
-            result = "wrong. Don't give up. Keep studying, and you'll get it right next time!"
-
-        return render_template("answer.html", result=result, answer=session["answer"])
+        remainder = user.levels.points % 5 
+        progress = remainder * 100 / 5
+        session["progress"] = progress
 
     else:
-        flash("That is an invalid method.")
+        result = "wrong. Don't give up. Keep studying, and you'll get it right next time!"
 
+    return render_template("answer.html", result=result, answer=session["answer"])
 
 
 
